d70eae346e05ad9ab0a4cc5119f87bfb9e1ebe1c
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-25 18:31:26 +0000

    jest commit (not working)

--- updateDoc.test.js
+++ updateDoc.test.js
@@ -1,7 +1,7 @@
 import { pushFakeDoc } from './testMethods/pushFakeDoc.js'
 import { updateSubscriber } from './testMethods/updateSubscriber.js';
 
-test('adds 1 + 2 to equal 3', async () => {
+test('Update subscriber doc', async () => {
     await pushFakeDoc();
     const result = expect(await updateSubscriber());
     expect(result).toBe(true);
